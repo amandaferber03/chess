fbe851521df1d50096129ff4b1ec73ff620a38d9
Amanda Ferber <aferber2@jhu.edu>
2022-04-20 00:36:35 -0400

    created necessary cpp files

---
+++ Pawn.cpp
@@ -0,0 +1,11 @@
+#include "Pawn.h"
+
+namespace Chess {
+  bool Pawn::legal_move_shape(const Position& start, const Position\
+& end) const {
+    /////////////////////////                                         
+    // [REPLACE THIS STUB] //                                         
+    /////////////////////////                                         
+    return false;
+  }
+}
