88a428ca54356e5f40d52c8a3a661e115269a6b2
Amanda Ferber <aferber2@jhu.edu>
2022-04-22 11:35:24 -0400

    we started legal_move_shape for pawn

--- Pawn.cpp
+++ Pawn.cpp
@@ -6,23 +6,36 @@ namespace Chess
     /////////////////////////                                         
     // [REPLACE THIS STUB] //                                         
     /////////////////////////                                         
+    bool white_turn_first = is_white() && start.second == 2;
+    bool black_turn_first = !(is_white()) && start.second == 7;
 
-    // if start == Position in map
-    // if Board::occ[start] == Pawn
+    if(start.first == end.first) {
+      if(white_turn_first) {
+	if(start.second == end.second + 1 || start.second == end.second + 2) {
+	  return true;
+	}
+      }
+      else if(is_white()) {
+	if(start.second == end.second + 1) {
+	  return true;
+	}
+      }
+      else if(black_turn_first) {
+	if(end.second == start.second - 1 || end.second == start.second -2) {
+	  return true;
+	}
+      }
+      else{//if black
+	if(end.second == start.second - 1) {
+	  return true;
+	}
+      }
+    }
+    return false;
 
     // checks if start and end Position are within the bounds of the board
     
     // checks if the player moved Pawn the correct way (1 space forward)
-    if (start.first == end.first && end.second == start.second + 1) {
-        return true
-      // }
-    // }
-    else {
-      // return false
-    // }
 
-
-    }
-    return false;
   }
 }
