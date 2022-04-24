60b21aa03f37938aaf1c77a09c6c1aadef042717
nicolesaade <nicolesaadea@gmail.com>
2022-04-24 12:56:03 -0400

    Modified the king class adding the legal move method (Nicole)

--- King.cpp
+++ King.cpp
@@ -3,10 +3,27 @@
 namespace Chess
 {
   bool King::legal_move_shape(const Position& start, const Position& end) const {
-    /////////////////////////                                         
-    // [REPLACE THIS STUB] //                                         
-    /////////////////////////                                         
-    return false;
+    //Horizontal and diagonal movement
+   	if (end.second == start.second + 1 || end.second == start.second - 1) {
+       //Allowing for horizontal movement
+       if (end.first == second.first) {
+           return true;
+       }
+       //Allowing for diagonal movement
+       if (end.first == second.first + 1 || end.first == second.first - 1) {
+           return true;
+       }
+   }
+ 
+   //Vertical movement
+   if (end.first == start.first + 1 || end.first == start.first - 1) {
+       //Making sure it is within the same line
+       if (end.second == second.second) {
+           return true;
+       }
+   }
+ 
+   return false;
   }
 
   char to_ascii() const {
