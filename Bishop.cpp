10926339b270a4e548424249cd4d902dcf244ccd
nicolesaade <nicolesaadea@gmail.com>
2022-04-24 12:53:26 -0400

    Applied Legal Move method to Bishop (Nicole)

--- Bishop.cpp
+++ Bishop.cpp
@@ -3,10 +3,13 @@
 namespace Chess
 {
   bool Bishop::legal_move_shape(const Position& start, const Position& end) const {
-    /////////////////////////
-    // [REPLACE THIS STUB] //
-    /////////////////////////
-    return false;
+    for (int i = 1; i < 7; i++) {
+       if ((end.first == start.first + i || end.first == start.first - i) && (end.second == start.second + i || end.second == start.second + i)) {
+           return true;
+       }
+   	}
+   	return false;
+ 	}
   }
 
   char to_ascii() const {
